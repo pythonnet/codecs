7e86fdbaf52c1a56cc6ad910e6cb61627f49a38f
Victor Milovanov <lost@losttech.software>
2020-04-09 13:38:09 -0700

    workaround for Python.NET not appending lib prefix to DLL name

--- tests/TestsRuntimeConfig.cs
+++ tests/TestsRuntimeConfig.cs
@@ -9,6 +9,14 @@ namespace Python.Runtime.Codecs
         {
             if (PythonEngine.IsInitialized)
                 return;
+
+            if (Path.IsPathFullyQualified(Runtime.PythonDLL)) return;
+
+            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows) && !Runtime.PythonDLL.StartsWith("lib"))
+            {
+                string dllExtension = RuntimeInformation.IsOSPlatform(OSPlatform.OSX) ? ".dylib" : ".so";
+                Runtime.PythonDLL = "lib" + Runtime.PythonDLL + dllExtension;
+            }
         }
     }
 }
